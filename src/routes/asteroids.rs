f60457875562a58cf7ab54251cef5253fcf7a92c
abominablepug <esnahoum@gmail.com>
2026-04-28 14:53:12 -0400

    feat: pagination using url params for asteroid endpoint

--- src/routes/asteroids.rs
+++ src/routes/asteroids.rs
@@ -1,13 +1,29 @@
 use crate::AppState;
 use crate::error::ApiError;
 use crate::models::asteroids::NeoResponse;
-use axum::response::Json;
-use axum::{Router, extract::State, routing::get};
+use axum::{
+    Router,
+    extract::{Query, State},
+    response::Json,
+    routing::get,
+};
+use serde::Deserialize;
 
-async fn get_asteroids(State(state): State<AppState>) -> Result<Json<NeoResponse>, ApiError> {
+#[derive(Deserialize)]
+struct Params {
+    page: Option<u32>,
+    size: Option<u32>,
+}
+
+async fn get_asteroids(
+    State(state): State<AppState>,
+    Query(params): Query<Params>,
+) -> Result<Json<NeoResponse>, ApiError> {
     let asteroids_data = reqwest::get(format!(
-        "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={}",
+        "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={}&page={}&size={}",
         &state.nasa_api_key,
+        params.page.unwrap_or(0),
+        params.size.unwrap_or(20)
     ))
     .await
     .map_err(|e| ApiError::Internal(format!("Failed to fetch data: {}", e)))?
